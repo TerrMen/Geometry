b9c75952b13e4bbdd33a54bca096d84e1f338afd
Kostya <koreshin67@gmail.com>
2024-04-18 20:47:42 +0300

    feat: add input of three coords

---
+++ main.jl
@@ -0,0 +1,35 @@
+### A Pluto.jl notebook ###
+# v0.19.41
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ dc315aaa-fda9-11ee-3951-41c7243b53ea
+print("A = ")
+
+# ╔═╡ e5359940-8445-454f-8a32-8383d8442b5c
+first_coords = split(readline())
+
+# ╔═╡ a16ad05d-c418-45d7-84be-f6f5da3116b2
+print("B = ")
+
+# ╔═╡ 34ce2855-8fd8-439e-8e7d-14a7138536cb
+second_coords = split(readline())
+
+# ╔═╡ 4af44adb-f5a9-409d-8bbc-93c8a03205d6
+print("C = ")
+
+# ╔═╡ 9b881c67-3776-4550-9bd9-3bdfbc788c49
+third_coords = split(readline())
+
+# ╔═╡ 122ffbb3-b504-4d65-b9af-68da2413c57a
+println(first_coords, second_coords, third_coords)
+
+# ╔═╡ Cell order:
+# ╠═dc315aaa-fda9-11ee-3951-41c7243b53ea
+# ╠═e5359940-8445-454f-8a32-8383d8442b5c
+# ╠═a16ad05d-c418-45d7-84be-f6f5da3116b2
+# ╠═34ce2855-8fd8-439e-8e7d-14a7138536cb
+# ╠═4af44adb-f5a9-409d-8bbc-93c8a03205d6
+# ╠═9b881c67-3776-4550-9bd9-3bdfbc788c49
+# ╠═122ffbb3-b504-4d65-b9af-68da2413c57a
